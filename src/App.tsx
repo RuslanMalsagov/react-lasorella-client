13849b2466adcc87db03be5ae11ac5815f187a82
Malsagov <rus.malsagov177@mail.ru>
2023-01-02 20:00:29 +0300

    реализовал компонент Header

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,6 @@
 import { Button } from './components/button';
 import { Input } from './components/input';
-import { InputSize } from './components/input-base';
 import { Spinner } from './components/spinner';
-import { createRef } from 'react';
 import { Header } from './components/header';
 
 export function App() {
