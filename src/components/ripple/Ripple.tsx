b8a83dd9504bdbeb3e8681c14d2223e80ae6e6ca
Malsagov <rus.malsagov177@mail.ru>
2023-01-10 22:24:58 +0300

    реализовал компонент Header

--- src/components/ripple/Ripple.tsx
+++ src/components/ripple/Ripple.tsx
@@ -25,7 +25,6 @@ export function Ripple({ duration = 850, color = CssVariable.ButtonRippleColor }
 
   const addRipple = (e: React.MouseEvent) => {
     const rippleContainer = e.currentTarget.getBoundingClientRect();
-
     const size = rippleContainer.width > rippleContainer.height ? rippleContainer.width : rippleContainer.height;
     const x = e.pageX - rippleContainer.left - size / 2;
     const y = e.pageY - rippleContainer.top - size / 2;
